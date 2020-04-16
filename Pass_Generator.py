40bb7161b2a316debd89c5c433381d3f30d5b5f3
ganchojr <carloscremigio@protonmail.ch>
2020-04-16 17:08:33 +0100

    First Commit

---
+++ Pass_Generator.py
@@ -0,0 +1,27 @@
+import random, string
+import sys
+import pyperclip
+
+print("-" * 50)
+print(" ")
+print("\t\tPassword Generator by @GanchoJr")
+print(" ")
+print("-" * 50)
+
+char_size = 1
+password = ""
+
+while char_size:
+    char_size = int(input("How many chars do you want in your password? Choose between 4 and 21: "))
+    if 4 <= char_size <= 21:
+        print("\nHere's your new Password: ")
+        password = ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(char_size))
+        print(" ")
+        print(password)
+        print(" ")
+        pyperclip.copy(password)
+        print("Already copied to clipboard, green light to use it!")
+        sys.exit(1)
+    else:
+        print("Please choose a number within the range. 4-21")
+    continue
