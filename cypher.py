716af70226668d1ed46d12db5c785d67c948a7f8
var-log <22357337+var-log@users.noreply.github.com>
2024-01-28 15:18:03 +0000

    Add files via upload

---
+++ cypher.py
@@ -0,0 +1,48 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(logo)
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+shift = shift % 26
+
+
+def caesar(direction, text, shift):
+  if direction == "encode":
+    rotated_alphabet = alphabet[shift:] + alphabet[:shift]
+  elif direction == "decode":
+    rotated_alphabet = alphabet[-shift:] + alphabet[:-shift]
+  else:
+    print("Wrong option.")
+    
+  buffer_text = list(text)
+  final_text = []
+  for letter in buffer_text:
+    if letter in alphabet:
+      buffer_position = alphabet.index(letter)
+      final_text.append(rotated_alphabet[buffer_position])
+    else:
+      final_text += letter
+  
+  final_text = ''.join(final_text)
+  print(final_text)
+
+caesar(direction, text, shift)
